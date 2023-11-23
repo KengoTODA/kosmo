eeb66e9692b39fc066e64c978f488c7a54cc2816
Kengo TODA <skypencil@gmail.com>
2023-11-23 09:47:30 +0800

    build: configure to upload build scans
    Signed-off-by: Kengo TODA <skypencil@gmail.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,14 @@
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
+    id("com.gradle.enterprise") version("3.15.1")
 }
 
 rootProject.name = "kosmo"
 include("backend")
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+    }
+}
