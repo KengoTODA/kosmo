2fcc3fce41e031b90fe139fc40d3dff138270028
Kengo TODA <skypencil@gmail.com>
2024-03-30 20:14:31 +0800

    implement a simple transaction management
    Signed-off-by: Kengo TODA <skypencil@gmail.com>

--- backend/src/main/kotlin/jp/skypencil/kosmo/backend/value/TransactionId.kt
+++ backend/src/main/kotlin/jp/skypencil/kosmo/backend/value/TransactionId.kt
@@ -3,7 +3,7 @@ package jp.skypencil.kosmo.backend.value
 import com.github.f4b6a3.uuid.UuidCreator
 import java.util.UUID
 
-class TransactionId(private val uuid: UUID) : Comparable<TransactionId> {
+data class TransactionId(private val uuid: UUID) : Comparable<TransactionId> {
     init {
         check(uuid.version() == 6) {
             "TransactionIdはTime-basedなUUIDである必要があります"
