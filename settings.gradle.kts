5cfbb049269e229aba4ee5c0f285fe19ad367c48
Kengo TODA <skypencil@gmail.com>
2024-04-24 05:52:29 +0800

    build: replace deprecated plugin with develocity plugin
    Signed-off-by: Kengo TODA <skypencil@gmail.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,14 @@
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
-    id("com.gradle.enterprise") version("3.17.2")
+    id("com.gradle.develocity") version("3.17.2")
 }
 
 rootProject.name = "kosmo"
 include("backend")
 
-gradleEnterprise {
+develocity {
     buildScan {
-        termsOfServiceUrl = "https://gradle.com/terms-of-service"
-        termsOfServiceAgree = "yes"
+        termsOfUseUrl = "https://gradle.com/terms-of-service"
+        termsOfUseAgree = "yes"
     }
 }
