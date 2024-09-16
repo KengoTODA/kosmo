4385703de426f21745582cc11ea5d5a8910ed393
Kengo TODA <skypencil@gmail.com>
2024-09-16 15:08:58 +0800

    chore(deps): update dependency com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin to v1.9.25-1.0.20

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,5 +10,5 @@ repositories {
 dependencies {
     implementation("com.diffplug.spotless:spotless-plugin-gradle:6.25.0")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.25")
-    implementation("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:1.9.23-1.0.20")
+    implementation("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:1.9.25-1.0.20")
 }
