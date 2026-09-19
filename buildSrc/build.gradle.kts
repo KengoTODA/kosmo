91adee355244d400fb7916541d4008c006951835
Kengo TODA <toda_k@henry.jp>
2026-09-19 08:09:12 +0800

    build(kotlin): update plugin to 2.4.20

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,6 +9,6 @@ repositories {
 
 dependencies {
     implementation("com.diffplug.spotless:spotless-plugin-gradle:8.10.2")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.10")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.20")
     implementation("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:2.3.5")
 }
