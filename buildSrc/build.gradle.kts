0cbdf85e4a452e9b1737d50693d40f4fe863d15a
Kengo TODA <toda_k@henry.jp>
2026-09-19 08:09:12 +0800

    build(ksp): update plugin to 2.3.12

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,5 +10,5 @@ repositories {
 dependencies {
     implementation("com.diffplug.spotless:spotless-plugin-gradle:8.10.2")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.20")
-    implementation("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:2.3.5")
+    implementation("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:2.3.12")
 }
