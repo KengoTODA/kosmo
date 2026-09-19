c045ef55024c93df376d103d0f727198b47ec23d
Kengo TODA <toda_k@henry.jp>
2026-09-19 08:09:12 +0800

    build(spotless): update plugin to 8.10.2

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,7 +8,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.diffplug.spotless:spotless-plugin-gradle:8.2.1")
+    implementation("com.diffplug.spotless:spotless-plugin-gradle:8.10.2")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.10")
     implementation("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:2.3.5")
 }
